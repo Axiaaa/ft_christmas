786b547582a918e01d9811b449edb595751a1c45
Nephtys <lucie.camerlynck@hotmail.com>
2024-12-20 17:41:19 +0100

    first commit

---
+++ ft_snowball.py
@@ -0,0 +1,35 @@
+import curses, random, time
+
+def snow_fall(stdscr):
+    curses.curs_set(0) 
+    stdscr.nodelay(1) 
+    stdscr.timeout(1) 
+
+
+    height, width = stdscr.getmaxyx()
+    snowflakes = []
+
+    while True:
+        x = random.randint(0, width - 1)
+        snowflakes.append((0, x))
+
+        stdscr.clear()
+
+        new_snowflakes = []
+        for y, x in snowflakes:
+            stdscr.addstr(y, x, "*")
+
+            if y + 1 < height:
+                new_snowflakes.append((y + 1, x))
+
+        snowflakes = new_snowflakes
+
+        stdscr.refresh()
+
+        key = stdscr.getch()
+        if key == ord('q'):
+            break
+
+        time.sleep(0.1)
+
+curses.wrapper(snow_fall)
