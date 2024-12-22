ae8283c40a58f173d216dad87fb33deeedbc8bc0
Nephtys <lucie.camerlynck@hotmail.com>
2024-12-22 22:21:55 +0100

    🎄🎄🎄

--- ft_snowball.py
+++ ft_snowball.py
@@ -2,9 +2,7 @@ import curses, random, time
 
 def snow_fall(stdscr):
     curses.curs_set(0) 
-    stdscr.nodelay(1) 
-    stdscr.timeout(1) 
-
+    stdscr.nodelay(1), stdscr.timeout(1) 
 
     height, width = stdscr.getmaxyx()
     snowflakes = []
@@ -12,24 +10,17 @@ def snow_fall(stdscr):
     while True:
         x = random.randint(0, width - 1)
         snowflakes.append((0, x))
-
         stdscr.clear()
-
         new_snowflakes = []
         for y, x in snowflakes:
             stdscr.addstr(y, x, "*")
-
             if y + 1 < height:
                 new_snowflakes.append((y + 1, x))
-
         snowflakes = new_snowflakes
-
         stdscr.refresh()
-
         key = stdscr.getch()
         if key == ord('q'):
             break
-
-        time.sleep(0.1)
+        time.sleep(0.3)
 
 curses.wrapper(snow_fall)
